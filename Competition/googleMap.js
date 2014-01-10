624c113446aeb958a486b3b20b87b33cb26c9739
francesr <frances.c.rucker@gmail.com>
2014-01-10 17:52:50 -0500

    trying to push

--- Competition/googleMap.js
+++ Competition/googleMap.js
@@ -1,25 +1,35 @@
 var myLatlng = new google.maps.LatLng(42.3584,-71.091);
 
-var marker;
+var map;
+var markers = [];
 function initialize() {
 	var mapOptions = {
 		center: myLatlng,
 		zoom: 14
 	};
-	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 	// To add the marker to the map, use the 'map' property
-	marker = new google.maps.Marker({
+	markers = new google.maps.Marker({
 	    position: myLatlng,
 	    map: map,
 	    animation: google.maps.Animation.DROP,
-	    draggable: true,
 	    title:"Hello World!"
 	});
 
-	google.maps.event.addListener(marker, 'click', toggleBounce);
+	google.maps.event.addListener(map, 'click', function(event) {
+	    addMarker(event.latLng);
+	});
 }
 	
-	
+// Add a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map,
+    draggable: true
+  });
+  markers.push(marker);
+}
 
 function toggleBounce() {
 
