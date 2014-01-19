fa2b5bd23611f23c353adfde118f9d9a364c267b
cmchang <changc@mit.edu>
2014-01-19 17:01:23 -0500

    Basic rails blog with bootstrap added, note currently only works on my desktop but wanted to save all of the files

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'c3b6ed5dcaee310d5265469778d8b31bc65ff7d627e4744fac853baa48fa6e3623a93f41ed37e4d5c1d4740867e2ae6ca149440212b5cc20bdbc0d0006c57878'
